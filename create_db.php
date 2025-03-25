0ec25f49f2d0dbfebead3ffb94ad58e3f002398d
Bbguss <mbbbdg@gmail.com>
2025-03-25 10:10:27 +0700

    update

--- create_db.php
+++ create_db.php
@@ -1,13 +1,39 @@
-<?php
-$servername = "localhost";
-$username = "root"; 
-$password = "";
-$dbname = "buku_tamu";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    </head>
+    <body>
+        <?php
+            include 'koneksi.php';
 
-// Koneksi ke MySQL
-$conn = new mysqli($servername, $username, $password);
+        // Gunakan database
+        $conn->select_db($dbname);
 
-// Cek koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
+        // Buat tabel keperluan
+        $sql = "CREATE TABLE IF NOT EXISTS keperluan (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            deskripsi VARCHAR(255) NOT NULL
+        )";
+        $conn->query($sql);
+
+        // Buat tabel tamu
+        $sql = "CREATE TABLE IF NOT EXISTS tamu (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nama VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            pesan TEXT NOT NULL,
+            keperluan_id INT NOT NULL,
+            tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (keperluan_id) REFERENCES keperluan(id) ON DELETE CASCADE
+        )";
+        $conn->query($sql);
+
+
+        echo "Database dan semua Tabel berhasil dibuat. Silakan <a href='tampil_tamu.php'>lihat tabel</a>";
+        $conn->close();
+        ?>
+        </body>
+        </html>
--- insert.php
+++ insert.php
@@ -1,9 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Buku Tamu</title>
-</head>
-<body>
-    <h1>Selamat Datang di Buku Tamu</h1>
-</body>
-</html>
+<?php
+include 'koneksi.php';
+
+// Insert ke tabel keperluan
+$sql = "INSERT INTO keperluan (deskripsi) VALUES 
+    ('Bisnis'), 
+    ('Wisata'), 
+    ('Kunjungan Keluarga')";
+$conn->query($sql);
+
+// Insert ke tabel tamu
+$sql = "INSERT INTO tamu (nama, email, pesan, keperluan_id) VALUES 
+    ('Bagus', 'gus@mail.com', 'Perihal kerjasama', 1),
+    ('Budi', 'budi@mail.com', 'Saya ingin reservasi.', 2),
+    ('Citra', 'citra@mail.com', 'Sangat puas dengan layanan.', 3)";
+$conn->query($sql);
+
+echo "Data awal berhasil ditambahkan✅.";
+$conn->close();
+?>
